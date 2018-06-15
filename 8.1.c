53e6036349a54157bf81a4f4e7cec35dd291b93c
dear-william <1501547345@qq.com>
2018-06-15 14:21:29 +0800

    homework

---
+++ 8.1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{ void swap(int *p1,int *p2);
+ int n1,n2,n3;
+ int *p1,*p2,*p3;
+ printf("input a integer n1:");
+ scanf("%d",&n1);
+ printf("input a integer n2:");
+ scanf("%d",&n2);
+ printf("input a integer n3:");
+ scanf("%d",&n3);
+ p1=&n1;
+ p2=&n2;
+ p3=&n3;
+ if(n1>n2) swap(p1,p2);
+ if(n1>n3) swap(p1,p3);
+ if(n2>n3) swap(p2,p3);
+ printf("Now,the order is:%d,%d,%d\n",n1,n2,n3);
+ return 0;
+ }
+ void swap(int *p1,int *p2)
+  {int p;
+   p=*p1; *p1=*p2; *p2=p;
+  }
